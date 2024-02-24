87a139382ec19194940dfdc908f4e35c8c5d9465
Dev-Alex23 <alexhinds1@outlook.com>
2024-02-24 21:24:35 +0000

    test: initial test setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/SetupTests.ts',
+    include: ['./src/tests/**/*.test.ts', './src/tests/**/*.test.tsx'],
+  },
   plugins: [react()],
   resolve: {
     alias: {
