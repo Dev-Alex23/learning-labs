8b4f1a135f9aa751c61c0ff9a5f2550f726e4f18
Alex Hinds <alex.hinds@weareadaptive.com>
2024-03-20 15:25:29 +0000

    style(font): add poppins fonts and remove scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ import withMT from '@material-tailwind/react/utils/withMT';
 export default withMT({
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 });
